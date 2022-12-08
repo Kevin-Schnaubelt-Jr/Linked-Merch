1299b5b4deffff3868f4e42cd91840f81556d34a
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2022-12-07 21:13:46 -0500

    progress capture

--- linked_project/static/app.js
+++ linked_project/static/app.js
@@ -9,22 +9,27 @@ const app = Vue.createApp({
                 "unique_key" : "",
                 "template_key": 0,
                 "author": "",
-            }
+            },
+            qrCount: 0
         }
     },
     methods: {
         loadUrls(){
             axios({
                 method: 'get',
-                url: 'api/v1/custom_urls'
+                url: '/api/v1/custom_urls/'
             }).then(response => {
                 this.urls = response.data
                 console.log(response.data)
                 this.loadCurrentUser()
                 }
-            )
+            ).catch(error => {
+    
+                console.log(error.response)
+             
+            })
         },
-        createToDo() {
+        createUrl() {
             
             axios({
                 method: 'post',
@@ -33,7 +38,7 @@ const app = Vue.createApp({
                     'X-CSRFToken': this.csrfToken
                 },
                 data: {
-                    "unique_key" : this.newUrl.unique_key,
+                    "unique_key" : this.randomKeyGenerator(),
                     "template_key": this.newUrl.template_key,
                     "author": this.currentUser.id,
                 }
@@ -51,9 +56,23 @@ const app = Vue.createApp({
                 method: 'get',
                 url: '/users/currentuser/'
             }).then(response => {
-                console.log('CU', response.data)
+                // console.log('CU', response.data)
                 this.currentUser = response.data
+                console.log('current user', this.currentUser)
+                this.qrCount = this.currentUser.urls_detail.length
+            }).catch(error => {
+              
+                console.log(error.response)
             })
+        },
+        randomKeyGenerator(){
+            let result = ''
+            let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+            let charactersLength = characters.length
+            for (let i = 0; i < 20; i++){
+                result += characters.charAt(Math.floor(Math.random() * charactersLength))
+            }
+            return result
         }
 
     },
@@ -63,7 +82,7 @@ const app = Vue.createApp({
     },
     mounted(){
         this.csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value
-      
+        console.log('MOUNTED')
        
     }
 })
