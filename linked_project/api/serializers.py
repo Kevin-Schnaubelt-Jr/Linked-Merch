d785da34410597bd0fcb2a4d190252de5b295602
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2022-12-20 20:53:44 -0500

    deployable. with console logs

--- linked_project/api/serializers.py
+++ linked_project/api/serializers.py
@@ -6,7 +6,7 @@ from custom_urls.models import URLS, EmergencyName, EmergencyPhoneNumbers, Emerg
 class NestedURLSSerializer(serializers.ModelSerializer):
     class Meta:
         model = URLS
-        fields = ('id', 'unique_key', 'template_key', 'author')
+        fields = ('id', 'unique_key', 'template_key', 'body_style', 'eye_style', 'eye_ball_style', 'body_color', 'bg_color', 'eye_1_color', 'eye_ball_1_color', 'author')
 
 class NestedUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,7 +38,7 @@ class URLSSerializer(serializers.ModelSerializer):
     address_detail = NestedEmergencyAddressSerializer(many=True, read_only=True, source='emergency_address')
     class Meta:
         model = URLS
-        fields = ('id', 'unique_key', 'template_key', 'author', 'author_detail',  'name_detail', 'phone_detail', 'address_detail')
+        fields = ('id', 'unique_key', 'template_key', 'body_style', 'eye_style', 'eye_ball_style', 'body_color', 'bg_color', 'eye_1_color', 'eye_ball_1_color', 'author', 'author_detail',  'name_detail', 'phone_detail', 'address_detail')
 
 class UserSerializer(serializers.ModelSerializer):
     urls_detail = NestedURLSSerializer(many=True, source='custom_urls', read_only=True)
--- linked_project/custom_urls/models.py
+++ linked_project/custom_urls/models.py
@@ -6,6 +6,13 @@ from users.models import CustomUser
 class URLS(models.Model):
     unique_key = models.CharField(max_length=200, unique=True)
     template_key = models.IntegerField()
+    body_style = models.CharField(max_length=50, default='circle' )
+    eye_style = models.CharField(max_length=50, default='frame6')
+    eye_ball_style = models.CharField(max_length=50, default='ball6')
+    body_color = models.CharField(max_length=50, default='', null=True)
+    bg_color = models.CharField(max_length=50, default='', null=True)
+    eye_1_color = models.CharField(max_length=50, default='', null=True)
+    eye_ball_1_color = models.CharField(max_length=50, default='', null=True)
     author = models.ForeignKey(CustomUser, related_name="custom_urls", on_delete=models.CASCADE)
 
     def __str__(self):
