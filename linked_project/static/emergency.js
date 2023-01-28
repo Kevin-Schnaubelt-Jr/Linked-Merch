4f4ebe038c45021d8049926d5a5187250ecc43f3
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2023-01-28 13:27:41 -0500

    lets gooo

--- linked_project/static/emergency.js
+++ linked_project/static/emergency.js
@@ -24,13 +24,25 @@ const emergency = Vue.createApp({
             emergencyNameInputField: '',
             emergencyNameDescriptorInputField: '',
 
+            emergencyPhoneInputField: '',
+            emergencyPhoneDescriptorInputField: '',
+
+            emergencyAddressDescriptorInputField: '',
+            emergencyAddressStreetField: '',
+            emergencyAddressCityField: '',
+            emergencyAddressStateField: '',
+            emergencyAddressZipCodeField: '',
+
 
             // bools
             createNameBool: true,
             displayQRCodeBool: true,
+            createPhoneBool: true,
+            createAddressBool: true,
 
             // format phone variable
             currentPhones: [],
+            formattedPhones: [],
             
         }
     },
@@ -52,7 +64,7 @@ const emergency = Vue.createApp({
                 console.log('test', document.querySelector('#page-key').innerHTML)
                 pageKey = document.querySelector('#page-key').innerHTML
                 for (i=0; i < this.gotQRCodes.length; i++){
-                    console.log('keys', this.gotQRCodes[i].unique_key)
+                    // console.log('keys', this.gotQRCodes[i].unique_key)
                     if (this.gotQRCodes[i].unique_key === pageKey){
                         console.log('SUCESSS', this.gotQRCodes[i])
     
@@ -102,8 +114,6 @@ const emergency = Vue.createApp({
                     }
                 }).then(response => {
                     console.log('posted', response)
-                    nameInputDiv.style.display = 'none'
-                    nameButton.style.display = 'block'
                     this.emergencyNameDescriptorInputField = ''
                     this.emergencyNameInputField = ''
                     this.loadQRCodes()
@@ -113,8 +123,92 @@ const emergency = Vue.createApp({
                     console.log('create name errors', error.response)
                 })
 
+                nameInputDiv.style.display = 'none'
+                nameButton.style.display = 'block'
+
             }
             this.createNameBool = !this.createNameBool
+        },
+        createPhone(key){
+            let nameInputDiv = document.querySelector('#make-new-emergency-phone-div')
+            let nameButton = document.querySelector('#new-emergency-phone-button')
+            if (this.createPhoneBool){
+                console.log('true')
+                nameInputDiv.style.display = 'block'
+                nameButton.style.display = 'none'
+                
+            }
+            else {
+                console.log('key', typeof key, key)
+                axios({
+                    method: 'post',
+                    url: '/api/v1/emergency_phones/',
+                    headers: {
+                        'X-CSRFToken': this.csrfToken
+                    },
+                    data: {
+                        descriptor: this.emergencyPhoneDescriptorInputField,
+                        phone_number: '+1' + this.emergencyPhoneInputField,
+                        qr_code: key
+                    }
+                }).then(response => {
+                    console.log('posted', response)
+                    
+                    this.emergencyPhoneDescriptorInputField = ''
+                    this.emergencyPhoneInputField = ''
+                    this.loadQRCodes()
+        
+                }).catch(error => {
+              
+                    console.log('create phone errors', error.response)
+                })
+                nameInputDiv.style.display = 'none'
+                nameButton.style.display = 'block'
+
+            }
+            this.createPhoneBool = !this.createPhoneBool
+        },
+        createAddress(key){
+            let nameInputDiv = document.querySelector('#make-new-emergency-address-div')
+            let nameButton = document.querySelector('#new-emergency-address-button')
+            if (this.createAddressBool){
+                console.log('true')
+                nameInputDiv.style.display = 'block'
+                nameButton.style.display = 'none'
+                
+            }
+            else {
+                console.log('key', typeof key, key)
+                axios({
+                    method: 'post',
+                    url: '/api/v1/emergency_address/',
+                    headers: {
+                        'X-CSRFToken': this.csrfToken
+                    },
+                    data: {
+                        descriptor: this.emergencyAddressDescriptorInputField,
+                        street: this.emergencyAddressStreetField,
+                        city: this.emergencyAddressCityField,
+                        state: this.emergencyAddressStateField,
+                        zip_code: this.emergencyAddressZipCodeField,
+                        qr_code: key
+                    }
+                }).then(response => {
+                    console.log('posted', response)
+                    
+                    this.emergencyPhoneDescriptorInputField = ''
+                    this.emergencyPhoneInputField = ''
+                    this.loadQRCodes()
+        
+                }).catch(error => {
+              
+                    console.log('create phone errors', error.response)
+                })
+                nameInputDiv.style.display = 'none'
+                nameButton.style.display = 'block'
+
+            }
+            this.createAddressBool = !this.createAddressBool
         },
         loadCurrentUser(){
             console.log('lets gooo')
@@ -137,7 +231,7 @@ const emergency = Vue.createApp({
                 this.gotQRCodes = response.data
                 console.log('get QR codes', this.gotQRCodes)
                 this.displayQRCode()
-                this.formatPhones()
+                this.storePhones()
                 
                 }
             ).catch(error => {
@@ -244,24 +338,57 @@ const emergency = Vue.createApp({
             });
             return qrCode
         },
-        formatPhones(){
-            // this.currentPhones
+        storePhones(){
+            // loop through qr codes and grab the phone numbers of the current page and store them in variable
             pageKey = document.querySelector('#page-key').innerHTML
-            console.log('format key', document.querySelector('#page-key').innerHTML)
-            console.log('fuck', this.gotQRCodes)
             for (let i=0; i < this.gotQRCodes.length; i++){
-                console.log('loopin', this.gotQRCodes[i].unique_key)
                 if (this.gotQRCodes[i].unique_key == pageKey){
                     for (let x=0; x < this.gotQRCodes[i].phone_detail.length; x++){
-                        // console.log('in loop', this.gotQRCodes[i].phone_detail[x])
                         this.currentPhones.push(this.gotQRCodes[i].phone_detail[x].phone_number)
                     }
-                    console.log('phone list', typeof this.currentPhones, this.currentPhones)
                     
-
                 }
             }
+            this.formatPhones()
+        },
+        formatPhones(){
+            // loop though the current phones, then loop though the individual phone string.
+            // build a formatted string as you do so and then store it in a new variable to loop through on the dom. 
+            console.log('current phones', this.currentPhones)
+            let new_string_build = ''
+            for (let i=0; i < this.currentPhones.length; i++){
+                new_string_build = '('
+                console.log('currnet stringlength', this.currentPhones[i].length)
+                for (let x=0; x < this.currentPhones[i].length; x++){
+                    // console.log(this.emergencyPhones[i].phone_number[x])
+                    if (x > 1){
+                        new_string_build += this.currentPhones[i][x]
+                    }
+                    if (x == 4){
+                        new_string_build += ') '
+                    }
+                    if (x == 7){
+                        new_string_build += '-'
+                    }
+                }
+                this.formattedPhones.push(new_string_build)
+            }
+            console.log('emergency phone format', this.formattedPhones)
 
+
+        },
+        deleteEmergency(url, id){
+            
+            axios({
+                method: 'delete',
+                url: '/api/v1/' + url + id,
+                headers: {
+                    'X-CSRFToken': this.csrfToken
+                }
+            }).then(response => {
+                console.log('name deleted')
+                this.loadQRCodes()
+            })
         }
     },
     created: function() {
